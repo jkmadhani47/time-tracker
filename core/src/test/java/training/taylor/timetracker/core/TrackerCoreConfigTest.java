1c2bacfe83b41d1d4d391eb6db4d410f072ff12f
jigartech <jmadhani>
2022-05-30 21:14:55 -0500

    new changes

--- core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
@@ -14,8 +14,3 @@ import java.util.List;
 /**
  * Created by Jason on 6/20/2015.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TrackerCoreConfig.class)
-public class TrackerCoreConfigTest {
-
-}
